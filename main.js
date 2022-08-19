94c4bcf4b2fabceab75c45194cc9c31b290790bf
angel <sandex7@protonmail.com>
2022-08-19 15:38:07 -0500

    estilos para optimizar falta componentes

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
-//cambiar tema a dark
-$$('.theme').forEach(e => {
-    e.addEventListener('click',()=>{
-        if ($('.acerca').classList.contains('light')) {
-            $('.acerca').style.backgroundColor = '#222'
-            $('.acerca').style.color = '#fff'
-            $('.acerca').classList.remove('light')
-            $('.acerca').classList.add('dark')
-            $('.acerca .fa-sun').style.display = 'block'
-            $('.acerca .fa-moon').style.display = 'none'
-
-        } else if($('.acerca').classList.contains('dark')){
-            $('.acerca').style.backgroundColor = '#fff'
-            $('.acerca').style.color = '#222'
-            $('.acerca').classList.remove('dark')
-            $('.acerca').classList.add('light')
-            $('.acerca .fa-sun').style.display = 'none'
-            $('.acerca .fa-moon').style.display = 'block'
-        }
-    })
+let boton = document.querySelector("label");
+boton.addEventListener('click',()=>{
+	document.querySelector('body').classList.toggle('dark')
 })
